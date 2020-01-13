f6fe2e74bde5e9e5dd13369a0f4e56f5b5ab1209
Evgesha1880 <eva.tarabrina.110@gmail.com>
2020-01-13 17:08:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int kw = 0;
+        String k ;
+        String n ;
+        String b ;
+        if (k) {
+            if (k){
+               kw+=0;
+            }
+            else if (n){
+                kw+=1;
+            }
+            else (b) {
+                kw-=1;
+            }
+        }
+        if (n) {
+            if (k){
+                kw-=1;
+            }
+            else if (n){
+                kw+=0;
+            }
+            else (b) {
+                kw += 1;
+            }
+        }
+        if (b) {
+            if (k){
+                kw+=1;
+            }
+            else if (n){
+                kw-=1;
+            }
+            else (b) {
+                kw+=0;
+            }
+        }
+        System.out.println(kw);
+        for (kw=0; kw++;);
+    }
+}
