64287e04b0f91d14aa125f817874cd9619ffdf83
tarabrin <eva.tarabrina.110@gmail.com>
2020-01-19 21:25:16 +0300

    game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,51 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            game();
+        }
+    }
+
+    public static void game() {
         int kw = 0;
-        String k ;
-        String n ;
-        String b ;
-        if (k) {
-            if (k){
-               kw+=0;
-            }
-            else if (n){
-                kw+=1;
-            }
-            else (b) {
-                kw-=1;
-            }
+        System.out.println("Игрок 1, выбери: \n" +
+                "1 камень \n" +
+                "2 ножницы \n" +
+                "3 бумага \n");
+        Scanner scanner = new Scanner(System.in);
+        int p1 = scanner.nextInt();
+
+        System.out.println("Игрок 2, выбери: \n" +
+                "1 камень \n" +
+                "2 ножницы \n" +
+                "3 бумага \n");
+        Scanner scanner1 = new Scanner(System.in);
+        int p2 = scanner1.nextInt();
+
+        System.out.println("Играем");
+
+        if (p1 == 1) {
+            if (p2 == 1) { }
+            else if (p2 == 2) { kw++; }
+            else if (p2 == 3) { kw--; }
         }
-        if (n) {
-            if (k){
-                kw-=1;
-            }
-            else if (n){
-                kw+=0;
-            }
-            else (b) {
-                kw += 1;
-            }
+        if (p1 == 2) {
+            if (p2 == 1) { kw--; }
+            else if (p2 == 2) { }
+            else if (p2 == 3) { kw++;}
         }
-        if (b) {
-            if (k){
-                kw+=1;
+        if (p1 == 3) {
+            if (p2 == 1) {
+                kw++;
+            } else if (p2 == 2) {
+                kw--;
+            } else if (p2 == 3) {
             }
-            else if (n){
-                kw-=1;
-            }
-            else (b) {
-                kw+=0;
-            }
+            System.out.println(kw);
         }
-        System.out.println(kw);
-        for (kw=0; kw++;);
     }
 }
