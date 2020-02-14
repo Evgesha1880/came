1d1248e62565a11ff18657c1127efe01ba5bbb41
Evgesha1880 <eva.tarabrina.110@gmail.com>
2020-02-14 19:35:16 +0300

    14.02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,34 +5,59 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Привет, Дружок\n"+
                 "    (⌒ω⌒)\n"+
                 "Желаю тебя удачкки\n"+
-                "   ＼(≧▽≦)／");
-        game();
+                "   ＼(≧▽≦)／\n" );
+        System.out.println("Для начала игры выбери режим:\n"+
+                "1 Игра с другом \n"+
+                "2 Игра с компьютером");
+        Scanner scanner = new Scanner(System.in);
+        int p1 = scanner.nextInt();
+        if (p1 ==1) {
+        go();
+        }
+        if (p1 == 2){
+            go2();
+        }
+    }
+    public static int random(int a, int b){
+        int random_number1 = a + (int) (Math.random() * b);
+        System.out.println(random_number1);
 
+        return random_number1;
+    }
+    public static void go() {
+        System.out.println("Для начала видите имена\n");
+        Scanner p1 = new Scanner(System.in);
+        String name1 = p1.nextLine();
+        Scanner p2 = new Scanner(System.in);
+        String name2 = p2.nextLine();
+
+        game();
         System.out.println("Хотите ли вы продолжить игру?");
         Scanner ch = new Scanner(System.in);
         String playnext = ch.nextLine();
         if (playnext.equals("да")) {
             go();
         }
-        if (playnext.equals("нет")) {
-            System.out.println("Спасибо, что были с нами))\n"+
-                    "        ♡(≧◡≦)♡");
+       else if (playnext.equals("нет")) {
+            System.out.println("Спасибо, что были с нами))\n" +"         ♡(≧◡≦)♡");
         }
 
     }
-    public static void go() {
-        game();
+    public static void go2() {
+        gamecom();
         System.out.println("Хотите ли вы продолжить игру?");
         Scanner ch = new Scanner(System.in);
         String playnext = ch.nextLine();
         if (playnext.equals("да")) {
-            go();
+            go2();
         }
-        if (playnext.equals("нет")) {
-            System.out.println("Спасибо, что были с нами))");
+        else if (playnext.equals("нет")) {
+            System.out.println("Спасибо, что были с нами))\n" +
+                               "         ♡(≧◡≦)♡");
         }
 
     }
@@ -53,6 +78,98 @@ public class Main {
                 "3 бумага \n");
         Scanner scanner1 = new Scanner(System.in);
         int p2 = scanner1.nextInt();
+
+        if (p1 == 1) {
+            if (p2 == 1) {
+                N++;
+            } else if (p2 == 2) {
+                kw++;
+            } else if (p2 == 3) {
+                L++;
+            }
+        }
+
+        if (p1 == 2) {
+            if (p2 == 1) {
+                L++;
+            } else if (p2 == 2) {
+                N++;
+            } else if (p2 == 3) {
+                kw++;
+            }
+        }
+
+        if (p1 == 3) {
+            if (p2 == 1) {
+                kw++;
+            } else if (p2 == 2) {
+                L++;
+            } else if (p2 == 3) {
+                N++;
+            }
+        }
+
+        if (kw == 1) {
+            System.out.println("Вы выиграли \n" +
+                    "░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░\n"+
+                    "░░░▄▄▄▄████████████▀▀█▄▄▄▄░\n"+
+                    "░░░█▄░░████████████░░█░░░█░\n"+
+                    "░░░░█░░████████████░▄█░░█░░\n"+
+                    "░░░░░▀▄░██████████░░█░▄▀░░░\n"+
+                    "░░░░░░░▀▀████████▀░█▀▀░░░░░\n"+
+                    "░▄░░░░░░░░▀█████▀▄▀░░░░░▄█▄\n"+
+                    "▀█▀░░░░░░░░░▀███▀░░░░░░░░▀░\n"+
+                    "░░░░░░░▄░░░░░░█░░░░░░░░░░░░\n"+
+                    "░░░░░░▀█▀░░░░░█░░░░░░░░░░░░\n"+
+                    "░░░░░░░░░░░░░▄█▄░░░░░░░░░░░\n"+
+                    "░░░░░░░░░░▄▄▄███▄▄▄░░░░░░░░\n"+
+                    "░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░\n"+
+                    "░░░░░░░███████████████░░░░░\n"+
+                    "░░░░░░░████▀▀▀▀▀▀▀████░░░░░\n"+
+                    "░░░░░░░███░░░░░░░░░███░░░░░\n"+
+                    "░░░░░░░████▄▄▄▄▄▄▄████░░░░░\n"+
+                    "░░░░░░░███████████████░░░░░\n"+
+                    "░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░\n");
+        } else if (L == 1) {
+            System.out.println("Вы проиграли\n"+
+                    "░░░░░░░░░░░█████████████░░░░░░\n" +
+                    "░░░░░░░░░███░███░░░░░░██░░░░░░\n" +
+                    "░░░░██████░░░░██░██████████░░░\n" +
+                    "░░░█████░░░░░░████░░░░░░░██░░░\n" +
+                    "░░██░░░░░░░░░░██░░███████████░\n" +
+                    "░░██░░░░░░░░░░░███░░░░░░░░░██░\n" +
+                    "░░██░░░░░░░░░░░███░██████████░\n" +
+                    "░░██░░░░░░░░░░░░████░░░░░░░██░\n" +
+                    "░░██░░░░░░░░░░░░░███░█████░░██\n" +
+                    "░░░██████░░░░░░░░░░████░░░░░██\n" +
+                    "░░░░░░░░███░░░░░░░░░░░░█████░░\n" +
+                    "░░░░░░░░░███░░░░░░░██████░░░░░\n" +
+                    "░░░░░░░░░░░██░░░░░░██░░░░░░░░░\n" +
+                    "░░░░░░░░░░░░███░░░░░██░░░░░░░░\n" +
+                    "░░░░░░░░░░░░░░██░░░░██░░░░░░░░\n" +
+                    "░░░░░░░░░░░░░░░███░░░██░░░░░░░\n" +
+                    "░░░░░░░░░░░░░░░░░██░░░██░░░░░░\n" +
+                    "░░░░░░░░░░░░░░░░░██░░░██░░░░░░\n" +
+                    "░░░░░░░░░░░░░░░░░░██░██░░░░░░░\n" +
+                    "░░░░░░░░░░░░░░░░░░████░░░░░░░░");
+        } else if (N == 1) {
+            System.out.println("Ничья");
+        }
+    }
+    public static void gamecom() {
+        int kw = 0;
+        int L = 0;
+        int N = 0;
+        System.out.println("Итак, теперь выбери: \n" +
+                "1 камень \n" +
+                "2 ножницы \n" +
+                "3 бумага \n");
+        Scanner scanner = new Scanner(System.in);
+        int p1 = scanner.nextInt();
+
+        System.out.println("Твой противник выбирает");
+
+        int p2 = random(1, 3);
         if (p1 == 1) {
             if (p2 == 1) {
                 N++;
@@ -128,10 +245,5 @@ public class Main {
         }
     }
 
-public static void random(){
-        int a=1;
-        int b=3;
-    int random_number1 = a + (int) (Math.random() * b);
 
-}
 }
